db3dd9ceb12ded8a6584a4d22b6710470973c1f4
Le Khanh Dang <khanhdang1026@gmail.com>
2026-06-02 21:02:54 +0700

    Merge Nhanbranch

---
+++ Backend/DockerFile/TuneVault.Application/UseCases/Auth/RegisterCommand.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using TuneVault.Application.DTOs;
+namespace TuneVault.Application.UseCases.Auth;
+
+//<summary>
+// Command để xử lý đăng ký người dùng mới
+// Trả về AuthResponseDTO chứa token và thông tin người dùng nếu đăng ký thành công
+//</summary>
+public record RegisterCommand(
+    string UserName,
+    string Email,
+    string Password,
+    string? Phone
+) : IRequest<AuthResponseDTO>;
