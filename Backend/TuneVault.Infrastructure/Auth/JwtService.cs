e45c90976e02c4f13eed0ed4e08d329727126d77
Le Khanh Dang <khanhdang1026@gmail.com>
2026-06-02 18:18:36 +0700

    Fix bug JwtService + Repository

--- Backend/TuneVault.Infrastructure/Auth/JwtService.cs
+++ Backend/TuneVault.Infrastructure/Auth/JwtService.cs
@@ -29,8 +29,8 @@ public class JwtService : IJwtService
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])!);
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
         var token = new JwtSecurityToken(
-            issuer: _config["Jwt: Issuer"],
-            audience: _config["Jwt: Audience"],
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
             claims: claims,
             expires: DateTime.UtcNow.AddHours(1),
             signingCredentials: creds
