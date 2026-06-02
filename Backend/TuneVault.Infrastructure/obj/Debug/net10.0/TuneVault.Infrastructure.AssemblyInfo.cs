401a1ea52de43d6710f0cf8bc3acd51235ac7abb
ThanhNhan65 <3124560065@sv.sgu.edu.vn>
2026-06-02 12:23:09 +0700

    copy branch DangKhanh

--- Backend/TuneVault.Infrastructure/obj/Debug/net10.0/TuneVault.Infrastructure.AssemblyInfo.cs
+++ Backend/TuneVault.Infrastructure/obj/Debug/net10.0/TuneVault.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe3e0273eea78bed6107ba910d9a4f9c1539cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945835443865745eb62e8fd3678bef19f6cf0945")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
