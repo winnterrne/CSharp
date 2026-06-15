a27bce93de4ce6c0f74edfc59f2af41d4b2481da
Ngoc Vinh <phvinh1206@gmail.com>
2026-06-16 01:40:12 +0700

    capnhat file

--- Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60042be485b1cf2adaa49849c6169cfd6745293d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153ed4cef1cce9cebabd6dd3636445f1940083fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
