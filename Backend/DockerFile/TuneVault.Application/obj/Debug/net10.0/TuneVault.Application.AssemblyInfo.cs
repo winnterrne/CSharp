41d38d4c412975601383a0fa0354a52d7a83d6e6
Le Khanh Dang <khanhdang1026@gmail.com>
2026-06-15 21:59:34 +0700

    Chỉnh sửa SideBar

--- Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc78ff5691c9c89b9e1ec91359e9a284874264c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d635b6fdc77c93bae2dd2fa432d4f9b5663953")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
