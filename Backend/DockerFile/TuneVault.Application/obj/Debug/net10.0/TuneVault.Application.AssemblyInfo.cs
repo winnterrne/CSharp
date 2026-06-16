408e4f3ba85c08b99424bdf92434f6d91c4e46ea
Le Khanh Dang <khanhdang1026@gmail.com>
2026-06-16 19:14:31 +0700

    Save local changes before merge

--- Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29ca90d4ad2ccbc5672adfc20e3633b952d2aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14940918d36729207472d0ccba6751b4c4b57ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
