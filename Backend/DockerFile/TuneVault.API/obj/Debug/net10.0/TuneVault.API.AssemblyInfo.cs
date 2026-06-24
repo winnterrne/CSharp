28ac7a1ab3b96ce78c33e76c6bd5bc7104cce04b
ThanhNhan65 <3124560065@sv.sgu.edu.vn>
2026-06-24 18:00:44 +0700

    checkout file của dangkhanh

--- Backend/DockerFile/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420965908d0ab35c1cc261d647131d21616b0d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee094b69c8eecd683e9a1694a677375639a1bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.Application/obj/Debug/net10.0/TuneVault.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420965908d0ab35c1cc261d647131d21616b0d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee094b69c8eecd683e9a1694a677375639a1bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
