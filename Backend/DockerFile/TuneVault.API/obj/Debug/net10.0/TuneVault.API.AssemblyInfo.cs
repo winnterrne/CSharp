f3233cf01c17ba60b3a82ee91679c27967a00f89
ThanhNhan65 <3124560065@sv.sgu.edu.vn>
2026-06-25 17:14:42 +0700

    Hoàn thiện đồ án

--- Backend/DockerFile/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
+++ Backend/DockerFile/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7594e76a4e613dbc58c90671b4bf769ce38db7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15204056797800601f42119cd58b687956307a18")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
